d4923447bbbcfcbea7e33cb2adfbe2595280dd2f
Wobear <122598132+Wobear@users.noreply.github.com>
2023-11-29 14:40:56 +0800

    Sounds update
    Some sounds implemented

---
+++ Assets/TestSound.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSound : MonoBehaviour
+{
+    public void OnExit()
+    {
+        Debug.Log("pointer exit");
+    }
+        
+}
