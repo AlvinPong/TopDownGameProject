89d7cf8d9e83d9d3d6026157bb8a2285fb23bb4e
AlvinPong <122720350+AlvinPong@users.noreply.github.com>
2024-02-22 15:34:09 +0800

    pre alpha 22 Feb
    pre alpha with bug fixes

--- Assets/GameStart.cs
+++ Assets/GameStart.cs
@@ -10,10 +10,16 @@ public class GameStart : MonoBehaviour
     public string PlayerMaxArmor = "PlayerMaxArmor";
     public string PlayerBombs = "PlayerCurrentBombs";
     public string PlayerMaxBombs = "PlayerMaxBombs";
+
+    public string HealthUpgrade = "ShopHealthUpgrade";
+    public string ArmorUpgrade = "ShopArmorUpgrade";
+    public string BombUpgrade = "ShopBombUpgrade";
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerSetUp();
+        ShopSetUp();
     }
 
     // Update is called once per frame
@@ -29,5 +35,11 @@ public class GameStart : MonoBehaviour
         PlayerPrefs.SetFloat(PlayerMaxArmor, 3);
         PlayerPrefs.SetFloat(PlayerBombs, 0);
         PlayerPrefs.SetFloat(PlayerMaxBombs, 0);
+    }
+    public void ShopSetUp()
+    {
+        PlayerPrefs.SetInt(HealthUpgrade, 0);
+        PlayerPrefs.SetInt(ArmorUpgrade, 0);
+        PlayerPrefs.SetInt(BombUpgrade, 0);
     }
 }
